279c077c1f75ce555522da0ecc2b09c5c13a5492
ljsf <2742070027@qq.com>
2019-09-11 17:15:04 +0800

    second commit

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
-/**
- * @Author: lijun
- * @Date:   2019-Sep-11
- * @Email:  FlydomTeam@163.com
- * @Project: FlyDom
- * @Filename: index.js
+// /**
+//  * @Author: lijun
+//  * @Date:   2019-Sep-11
+//  * @Email:  FlydomTeam@163.com
+//  * @Project: FlyDom
+//  * @Filename: index.js
  * @Last modified by:   lijun
  * @Last modified time: 2019-Sep-11
- * @Copyright: @copyright Copyright ©️ SingleTeam
- */
-
-let it = 0;
-let mytext;
-function initialize() {
-    mytext = $("#typing").text();
-    var myheight = $("#typing").offsetHeight;
-    $("#typing").text("");
-    $("#typing").css("height", myheight);
-    $("#typing").show();
-    typeit();
-}
-
-let t=0;
-function typeit() {
-    $("#typing").append(mytext.charAt(it));
-    if (it < mytext.length - 1) {
-        it++
-        t = setTimeout("typeit()", 100);
-    }
-    else
-        clearTimeout(t);
-}
-
-$(document).ready(function () {
-    initialize();
-});
+//  * @Copyright: @copyright Copyright ©️ SingleTeam
+//  */
+//
+// let it = 0;
+// let mytext;
+// function initialize() {
+//     mytext = $("#typing").text();
+//     var myheight = $("#typing").offsetHeight;
+//     $("#typing").text("");
+//     $("#typing").css("height", myheight);
+//     $("#typing").show();
+//     typeit();
+// }
+//
+// let t=0;
+// function typeit() {
+//     $("#typing").append(mytext.charAt(it));
+//     if (it < mytext.length - 1) {
+//         it++
+//         t = setTimeout("typeit()", 100);
+//     }
+//     else
+//         clearTimeout(t);
+// }
+//
+// $(document).ready(function () {
+//     initialize();
+// });
